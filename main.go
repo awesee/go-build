d475985be3786976ced270d6b36b4a8ce310d6de
openset <openset.wang@gmail.com>
2021-03-22 20:46:07 +0800

    U: packageName

--- main.go
+++ main.go
@@ -2,32 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
-	dir, err := os.Getwd()
-	checkErr(err)
-	cmdName := filepath.Base(dir)
+	packageName, err := exec.Command("go", "list").Output()
+	cmdName := strings.TrimSpace(string(packageName))
+	if err != nil || cmdName == "" {
+		cmdName, err = os.Getwd()
+		checkErr(err)
+	}
+	cmdName = filepath.Base(cmdName)
 	target := map[string][]string{
 		"darwin":  {"amd64"},
 		"windows": {"amd64"},
 		"linux":   {"amd64"},
 	}
-	err = os.Setenv("CGO_ENABLED", "0")
-	checkErr(err)
+	checkErr(os.Setenv("CGO_ENABLED", "0"))
 	for goos, goarchList := range target {
-		err = os.Setenv("GOOS", goos)
-		checkErr(err)
+		checkErr(os.Setenv("GOOS", goos))
 		goexe := ""
 		if goos == "windows" {
 			goexe = ".exe"
 		}
 		for _, goarch := range goarchList {
-			err = os.Setenv("GOARCH", goarch)
-			checkErr(err)
+			checkErr(os.Setenv("GOARCH", goarch))
 			fileName := fmt.Sprintf("%s.%s-%s%s", cmdName, goos, goarch, goexe)
 			fmt.Println("OS:", goos, "\tArch:", goarch, "\t", fileName)
 			err = exec.Command("go", "build", "-ldflags", "-s -w", "-o", fileName).Run()
@@ -38,6 +41,6 @@ func main() {
 
 func checkErr(err error) {
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 }
